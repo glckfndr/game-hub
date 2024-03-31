002459454d87bb16ca117cefe82b2e8ef35c33eb
glckfndr <obulan65@gmail.com>
2024-03-31 12:50:19 +0300

    feat: add images for GenreList, improve displaying

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import useGenres from "../hooks/useGenres";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { genres } = useGenres();
   return (
     <div>
-      <ul>
+      <List>
         {genres.map((gen) => (
-          <li>{gen.name}</li>
+          <ListItem key={gen.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(gen.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              ></Image>
+              <Text fontSize="lg">{gen.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </div>
   );
 };
