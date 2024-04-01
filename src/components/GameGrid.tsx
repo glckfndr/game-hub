32d26fdcaa338c9f3a7eedd50080d0f16b4545d0
glckfndr <obulan65@gmail.com>
2024-04-01 11:51:02 +0300

    refact: GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,28 +12,25 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px 10px 10px 0"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((sk) => (
-            <GameCardContainer key={sk}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {games.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px 10px 10px 0"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((sk) => (
+          <GameCardContainer key={sk}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {games.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
